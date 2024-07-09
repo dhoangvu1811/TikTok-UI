0daf9f013438bf0a7fed22661ec0d81897d64620
Đinh Hoàng Vũ <vudh.22itb@vku.udn.vn>
2024-07-09 21:05:34 +0700

    backup 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./conponents/Button";
 
 function App() {
   return (
     <div className="App">
+      <Button />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -13,8 +15,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
